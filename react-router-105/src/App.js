925e83a5f3819fcb09866a3395decf15b8b69e53
eastend-street <ss_1322irf@icloud.com>
2019-11-12 14:29:00 -0800

    implemented about, contact page

--- react-router-105/src/App.js
+++ react-router-105/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import IndexPage from "./components/IndexPage";
+import AboutPage from "./components/AboutPage";
+import ContactPage from "./components/ContactPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact render={()=> <div>This is index page.</div>}/>
+        <Route path="/" exact component={IndexPage}/>
+        <Route path="/about" exact component={AboutPage}/>
+        <Route path="/contact" exact component={ContactPage}/>
+        <Route>This is 404 page.</Route>
       </Switch>
     </BrowserRouter>
   );
